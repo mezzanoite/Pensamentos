112a655d8a685b4207e16e93a6deffdb503431f1
Usuário Convidado <Guest@pa06micro16.local>
2018-03-14 23:09:51 -0300

    Salvando ...

---
+++ Pensamentos/Quote.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+struct Quote: Codable { //Encodable, Decodable
+    
+    let quote: String
+    let author: String
+    let image: String
+    
+    var quoteFormatted: String {
+        return "〝" + quote + "〞"
+    }
+    
+    var authorFormatted: String {
+        return "- " + author + " -"
+    }
+}
